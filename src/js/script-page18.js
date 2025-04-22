88d2371f11c8ea02a9c9c56f737fe9e2a8b2a77b
foxstudent103376 <yashopua@gmail.com>
2025-04-22 11:45:08 +0300

    bb push

---
+++ src/js/script-page18.js
@@ -0,0 +1,11 @@
+//Реализация бургер-меню
+import { burger } from './functions/burger';
+
+//Language
+import { languageComponent } from './components/languageComponent.js';
+
+//Footer cookie
+import { footerCookie } from './components/footerCookieComponent';
+
+//btn scrollTop
+import { scrollTop } from './components/scrollTopComponent';
